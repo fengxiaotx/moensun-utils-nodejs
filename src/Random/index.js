965a130154e8b00d0fb5ce527e9c6a3d3ea0d2a2
fengxiaotx@163.com <yuxiaotx0323>
2016-02-24 16:38:18 +0800

    []

---
+++ src/Random/index.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Bane.Shi on 2/24/16.
+ */
+'use strict';
+
+function Random(){
+    if(!(this instanceof Random)){
+        return new Random();
+    }
+}
+
+Random.num = function(min,max){
+    return Math.round(Math.random()*(max-min)+min);
+}
